285475c8182473a39254c2f2c1cc8caa2c6b41c8
Francislainy Campos <francislainy.campos@hmhco.com>
2021-10-29 08:49:27 +0100

    Initial Commit

---
+++ adverts/advertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  advertsApp.swift
+//  adverts
+//
+//  Created by Campos, Francislainy on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct advertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
